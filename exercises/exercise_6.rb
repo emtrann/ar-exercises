0dba77107f3dfa7e96c3a2888516c2b58a6d066f
Michelle Tran <michelletraan@hotmail.com>
2020-11-10 23:43:30 +0000

    Exercise 6 complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 50)
+@store2.employees.create(first_name: "Tran", last_name: "Michelle", hourly_rate: 100)
+@store2.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 200)
+@store1.employees.create(first_name: "Meow", last_name: "Cats", hourly_rate: 20)
+@store2.employees.create(first_name: "Cookie", last_name: "Monster", hourly_rate: 300)
